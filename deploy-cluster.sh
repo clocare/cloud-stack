431a76975ca86742257f47d0f67f17ac83e73705
omarelsheekh <omarelsheekh98@gmail.com>
2021-05-31 04:39:18 +0200

    bash scripts

---
+++ deploy-cluster.sh
@@ -0,0 +1,14 @@
+echo "generating inventory and cluster.yml files"
+python3 script.py
+
+echo "installing docker on instances"
+ansible-playbook -u ubuntu -i inventory ansible-playbook.yml
+
+echo "downloading rke if doesn't exist"
+if ! [ -f rke ]; then
+  curl -o rke -s -L https://github.com/rancher/rke/releases/download/v1.2.8/rke_linux-amd64
+fi
+
+echo "creating k8s cluster"
+chmod +x rke
+./rke up
